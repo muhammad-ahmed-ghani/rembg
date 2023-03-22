7f66b085f7d8b108ccbd22bfed9bf7aa09dee4c0
Muhammad Ahmed <i.am.a.pakistani.programmer@gmail.com>
2023-03-22 13:31:42 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     install_requires=[
         "aiohttp~=3.8.1",
         "asyncer~=0.0.2",
-        "click~=8.1.3",
+        #"click~=8.1.3",
         "fastapi~=0.87.0",
         "filetype~=1.2.0",
         "pooch~=1.6.0",
